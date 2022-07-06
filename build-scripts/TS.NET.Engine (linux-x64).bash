f03a1e7718755abe9da69b720ed79914f3cd406a
macaba <1031306+macaba@users.noreply.github.com>
2022-07-06 21:20:49 +0100

    Added linux build scripts

---
+++ build-scripts/TS.NET.Engine (linux-x64).bash
@@ -0,0 +1,1 @@
+dotnet publish ../source/TS.NET.Engine/TS.NET.Engine.csproj -r linux-x64 -c Release --self-contained /p:PublishSingleFile=true /p:PublishTrimmed=true /p:IncludeNativeLibrariesForSelfExtract=true --output ../builds/linux-x64/TS.NET.Engine
