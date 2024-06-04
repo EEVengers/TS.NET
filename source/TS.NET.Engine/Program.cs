7070257722f52366e2c08a6ff095be28645a6c7f
John Simons <jammsimons@gmail.com>
2024-06-05 00:25:24 +0200

    TS.Net.Engine: Added commandline argument parameters for multidevice support
    This commit enables commandline arguments, specifically -i to select one of mulitple ThunderScopes on a host, -cport and -dport to set custom listener ports for the control and data plane servers, and -rev to select the PLL configuration dynamically instead of using build time preprocessor directives to switch between them.

--- source/TS.NET.Engine/Program.cs
+++ source/TS.NET.Engine/Program.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+﻿using System.CommandLine;
 using System.Diagnostics;
 using TS.NET;
 using TS.NET.Engine;
@@ -17,102 +16,144 @@ using (Process p = Process.GetCurrentProcess())
     p.PriorityClass = ProcessPriorityClass.High;
 
 #if DEBUG
-ThunderscopeSettings settings = ThunderscopeSettings.Default();
-var serializer = new YamlDotNet.Serialization.SerializerBuilder()
-    .WithNamingConvention(YamlDotNet.Serialization.NamingConventions.PascalCaseNamingConvention.Instance)
-    .Build();
-string yaml = serializer.Serialize(settings);
-File.WriteAllText("thunderscope (defaults).yaml", yaml);
+		ThunderscopeSettings settings = ThunderscopeSettings.Default();
+		var serializer = new YamlDotNet.Serialization.SerializerBuilder()
+			.WithNamingConvention(
+				YamlDotNet.Serialization.NamingConventions.PascalCaseNamingConvention.Instance
+			)
+			.Build();
+		string yaml = serializer.Serialize(settings);
+		File.WriteAllText("thunderscope (defaults).yaml", yaml);
 #endif
 
-IConfigurationBuilder configurationBuilder = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json");
-var configuration = configurationBuilder.Build();
-var thunderscopeSettings = ThunderscopeSettings.FromYamlFile("thunderscope.yaml");
+		IConfigurationBuilder configurationBuilder = new ConfigurationBuilder().AddJsonFile(
+			"appsettings.json"
+		);
+		var configuration = configurationBuilder.Build();
+		var thunderscopeSettings = ThunderscopeSettings.FromYamlFile("thunderscope.yaml");
 
-var loggerFactory = LoggerFactory.Create(configure =>
-{
-    configure
-        .ClearProviders()
-        .AddConfiguration(configuration.GetSection("Logging"))
-        .AddFile(configuration.GetSection("Logging"))
-        .AddSimpleConsole(options => { options.SingleLine = true; options.TimestampFormat = "HH:mm:ss "; });
-});
+		var loggerFactory = LoggerFactory.Create(configure =>
+		{
+			configure
+				.ClearProviders()
+				.AddConfiguration(configuration.GetSection("Logging"))
+				.AddFile(configuration.GetSection("Logging"))
+				.AddSimpleConsole(options =>
+				{
+					options.SingleLine = true;
+					options.TimestampFormat = "HH:mm:ss ";
+				});
+		});
 
-// Instantiate dataflow channels
-const int bufferLength = 120;       // 120 = about 1 seconds worth of samples at 1GSPS (each ThunderscopeMemory is 8388608 bytes), 120x = 1006632960
+		// Instantiate dataflow channels
+		const int bufferLength = 120; // 120 = about 1 seconds worth of samples at 1GSPS (each ThunderscopeMemory is 8388608 bytes), 120x = 1006632960
 
-ThunderscopeMemoryRegion memoryRegion = new(bufferLength);
-BlockingChannel<ThunderscopeMemory> inputChannel = new(bufferLength);
-for (uint i = 0; i < bufferLength; i++)
-    inputChannel.Writer.Write(memoryRegion.GetSegment(i));
-BlockingChannel<InputDataDto> processChannel = new();
-BlockingChannel<HardwareRequestDto> hardwareRequestChannel = new();
-BlockingChannel<HardwareResponseDto> hardwareResponseChannel = new();
-BlockingChannel<ProcessingRequestDto> processingRequestChannel = new();
-BlockingChannel<ProcessingResponseDto> processingResponseChannel = new();
+		ThunderscopeMemoryRegion memoryRegion = new(bufferLength);
+		BlockingChannel<ThunderscopeMemory> inputChannel = new(bufferLength);
+		for (uint i = 0; i < bufferLength; i++)
+			inputChannel.Writer.Write(memoryRegion.GetSegment(i));
+		BlockingChannel<InputDataDto> processChannel = new();
+		BlockingChannel<HardwareRequestDto> hardwareRequestChannel = new();
+		BlockingChannel<HardwareResponseDto> hardwareResponseChannel = new();
+		BlockingChannel<ProcessingRequestDto> processingRequestChannel = new();
+		BlockingChannel<ProcessingResponseDto> processingResponseChannel = new();
 
-Thread.Sleep(1000);
+		Thread.Sleep(1000);
 
-IThunderscope thunderscope;
-switch (thunderscopeSettings.Driver.ToLower())
-{
-    case "simulator":
-        {
-            var ts = new TS.NET.Driver.Simulator.Thunderscope();
-            thunderscope = ts;
-            break;
-        }
-    case "xdma":
-        {
-            // Find thunderscope
-            var devices = TS.NET.Driver.XMDA.Thunderscope.IterateDevices();
-            if (devices.Count == 0)
-                throw new Exception("No thunderscopes found");
-            var ts = new TS.NET.Driver.XMDA.Thunderscope();
-            ts.Open(devices[0], thunderscopeSettings.Calibration.ToDriver());
-            thunderscope = ts;
-            break;
-        }
-    default:
-        throw new ArgumentException($"{thunderscopeSettings.Driver} driver not supported");
+		IThunderscope thunderscope;
+		switch (thunderscopeSettings.Driver.ToLower())
+		{
+			case "simulator":
+			{
+				var ts = new TS.NET.Driver.Simulator.Thunderscope();
+				thunderscope = ts;
+				break;
+			}
+			case "xdma":
+			{
+				// Find thunderscope
+				var devices = TS.NET.Driver.XMDA.Thunderscope.IterateDevices();
+				if (devices.Count == 0)
+					throw new Exception("No thunderscopes found");
+				if (indexThunderscope > devices.Count-1) 
+					throw new Exception("Invalid index thunderscope " + indexThunderscope + ". Only " + devices.Count + " Thunderscopes connected.");
+				var ts = new TS.NET.Driver.XMDA.Thunderscope();
+				ts.Open(devices[indexThunderscope], thunderscopeSettings.Calibration.ToDriver(), hwRevision);
+				thunderscope = ts;
+				break;
+			}
+			default:
+				throw new ArgumentException($"{thunderscopeSettings.Driver} driver not supported");
+		}
+
+		// Start threads
+		SemaphoreSlim startSemaphore = new(1);
+
+		startSemaphore.Wait();
+		ProcessingThread processingThread =
+			new(
+				loggerFactory,
+				thunderscopeSettings,
+				processChannel.Reader,
+				inputChannel.Writer,
+				processingRequestChannel.Reader,
+				processingResponseChannel.Writer
+			);
+		processingThread.Start(startSemaphore);
+
+		startSemaphore.Wait();
+		HardwareThread hardwareThread =
+			new(
+				loggerFactory,
+				thunderscopeSettings,
+				thunderscope,
+				inputChannel.Reader,
+				processChannel.Writer,
+				hardwareRequestChannel.Reader,
+				hardwareResponseChannel.Writer
+			);
+		hardwareThread.Start(startSemaphore);
+
+		startSemaphore.Wait();
+		WaveformServer? waveformServer = null;
+		if (thunderscopeSettings.Twinlan)
+		{
+			waveformServer = new(
+				loggerFactory,
+				thunderscopeSettings,
+				System.Net.IPAddress.Any,
+				dataPort
+			);
+			waveformServer.Start();
+		}
+
+		ScpiServer scpiServer =
+			new(
+				loggerFactory,
+				System.Net.IPAddress.Any,
+				controlPort,
+				hardwareRequestChannel.Writer,
+				hardwareResponseChannel.Reader,
+				processingRequestChannel.Writer,
+				processingResponseChannel.Reader
+			);
+		scpiServer.Start();
+
+		bool loop = true;
+		while (loop)
+		{
+			var key = Console.ReadKey();
+			switch (key.Key)
+			{
+				case ConsoleKey.Escape:
+					loop = false;
+					break;
+			}
+		}
+
+		scpiServer.Stop();
+		waveformServer?.Stop();
+		hardwareThread.Stop();
+		processingThread.Stop();
+	}
 }
-
-// Start threads
-SemaphoreSlim startSemaphore = new(1);
-
-startSemaphore.Wait();
-ProcessingThread processingThread = new(loggerFactory, thunderscopeSettings, processChannel.Reader, inputChannel.Writer, processingRequestChannel.Reader, processingResponseChannel.Writer);
-processingThread.Start(startSemaphore);
-
-startSemaphore.Wait();
-HardwareThread hardwareThread = new(loggerFactory, thunderscopeSettings, thunderscope, inputChannel.Reader, processChannel.Writer, hardwareRequestChannel.Reader, hardwareResponseChannel.Writer);
-hardwareThread.Start(startSemaphore);
-
-startSemaphore.Wait();
-WaveformServer? waveformServer = null;
-if (thunderscopeSettings.Twinlan)
-{
-    waveformServer = new(loggerFactory, thunderscopeSettings, System.Net.IPAddress.Any, 5026);
-    waveformServer.Start();
-}
-
-ScpiServer scpiServer = new(loggerFactory, System.Net.IPAddress.Any, 5025, hardwareRequestChannel.Writer, hardwareResponseChannel.Reader, processingRequestChannel.Writer, processingResponseChannel.Reader);
-scpiServer.Start();
-
-bool loop = true;
-while (loop)
-{
-    var key = Console.ReadKey();
-    switch (key.Key)
-    {
-        case ConsoleKey.Escape:
-            loop = false;
-            break;
-    }
-}
-
-scpiServer.Stop();
-waveformServer?.Stop();
-hardwareThread.Stop();
-processingThread.Stop();
