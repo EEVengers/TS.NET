11005e4fa80a96db54bfab9ab87e1b5279ed1d07
Aleksa Bjelogrlic <aleksa@eevengers.com>
2024-07-26 00:56:44 -0400

    Copy over cal file from build directory so it doesn't get clobbered

--- build-scripts/TS.NET.Engine (linux-x64).bash
+++ build-scripts/TS.NET.Engine (linux-x64).bash
@@ -1,4 +1,12 @@
 version=$(cat ../source/TS.NET.Engine/TS.NET.Engine.csproj | grep -oPm1 "(?<=<Version>)[^<]+")
+
+cp ../builds/linux-x64/TS.NET.Engine/$version/thunderscope.yaml ../thunderscope.yaml
+cp ../builds/linux-x64/TS.NET.Engine/$version/appsettings.json ../appsettings.json
+
 dotnet publish ../source/TS.NET.Engine/TS.NET.Engine.csproj -r linux-x64 -c Release --self-contained /p:PublishSingleFile=true /p:PublishTrimmed=true /p:IncludeNativeLibrariesForSelfExtract=true --output ../builds/linux-x64/TS.NET.Engine/$version
-cp ../source/TS.NET.Engine/thunderscope.yaml ../builds/linux-x64/TS.NET.Engine/$version/thunderscope.yaml
-cp ../source/TS.NET.Engine/appsettings.json ../builds/linux-x64/TS.NET.Engine/$version/appsettings.json
+
+cp ../thunderscope.yaml ../builds/linux-x64/TS.NET.Engine/$version/thunderscope.yaml
+cp ../appsettings.json ../builds/linux-x64/TS.NET.Engine/$version/appsettings.json
+rm ../thunderscope.yaml
+rm ../appsettings.json
+
