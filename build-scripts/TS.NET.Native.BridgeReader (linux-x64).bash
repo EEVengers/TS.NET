7f7992dc82215f8a8f7a100df39aa318d69869a3
macaba <1031306+macaba@users.noreply.github.com>
2024-09-20 13:38:14 +0100

    Tidy up #2

--- build-scripts/TS.NET.Native.BridgeReader (linux-x64).bash
+++
@@ -1,2 +0,0 @@
-version=$(cat ../source/TS.NET.Native.BridgeReader/TS.NET.Native.BridgeReader.csproj | grep -oPm1 "(?<=<Version>)[^<]+")
-dotnet publish ../source/TS.NET.Native.BridgeReader/TS.NET.Native.BridgeReader.csproj -r linux-x64 -c Release /p:NativeLib=Shared /p:SelfContained=true --output ../builds/linux-x64/TS.NET.Native.BridgeReader/$version
