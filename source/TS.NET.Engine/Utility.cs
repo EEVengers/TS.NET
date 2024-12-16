d80c94e893ef844eab23d2980075ec05e8b00772
macaba <1031306+macaba@users.noreply.github.com>
2024-12-16 16:44:28 +0000

    Added more copy widths

--- source/TS.NET.Engine/Utility.cs
+++ source/TS.NET.Engine/Utility.cs
@@ -4,44 +4,57 @@ namespace TS.NET.Engine
 {
     public static class Utility
     {
+        record BenchmarkScenario(int Iterations, int CopySize);
+
         public static void MemoryBenchmark()
         {
-            const int runs = 10000;
-            Span<byte> source = new byte[ThunderscopeMemory.Length];
-            Span<byte> destination = new byte[ThunderscopeMemory.Length];
+            List<BenchmarkScenario> benchmarks =
+            [
+                new BenchmarkScenario(8000, 1 << 21),
+                new BenchmarkScenario(4000, 1 << 22),
+                new BenchmarkScenario(2000, 1 << 23),
+                new BenchmarkScenario(1000, 1 << 24),
+            ];
 
-            var position = Console.GetCursorPosition();
-            Console.WriteLine("Running warmup... 0%");
-            for (int i = 0; i < runs; i++)
+            foreach (var benchmark in benchmarks)
             {
-                source.CopyTo(destination);
+
+                Span<byte> source = new byte[benchmark.CopySize];
+                Span<byte> destination = new byte[benchmark.CopySize];
+
+                var position = Console.GetCursorPosition();
+                Console.WriteLine("Running warmup... 0%");
+                for (int i = 0; i < benchmark.Iterations; i++)
+                {
+                    source.CopyTo(destination);
+                    Console.SetCursorPosition(position.Left, position.Top);
+                    Console.WriteLine($"Running warmup... {(i / (double)benchmark.Iterations) * 100:F0}%");
+                }
                 Console.SetCursorPosition(position.Left, position.Top);
-                Console.WriteLine($"Running warmup... {(i / (double)runs) * 100:F0}%");
-            }
-            Console.SetCursorPosition(position.Left, position.Top);
-            Console.WriteLine("Running warmup... 100%");
+                Console.WriteLine("Running warmup... 100%");
 
-            position = Console.GetCursorPosition();
-            Console.WriteLine("Running benchmark... 0%");
-            List<double> results = new();
-            for (int i = 0; i < runs; i++)
-            {
-                var stopwatch = Stopwatch.StartNew();
-                source.CopyTo(destination);
-                stopwatch.Stop();
+                position = Console.GetCursorPosition();
+                Console.WriteLine("Running benchmark... 0%");
+                List<double> results = new();
+                for (int i = 0; i < benchmark.Iterations; i++)
+                {
+                    var stopwatch = Stopwatch.StartNew();
+                    source.CopyTo(destination);
+                    stopwatch.Stop();
 
-                results.Add(ThunderscopeMemory.Length / stopwatch.Elapsed.TotalSeconds);
+                    results.Add(benchmark.CopySize / stopwatch.Elapsed.TotalSeconds);
 
+                    Console.SetCursorPosition(position.Left, position.Top);
+                    Console.WriteLine($"Running benchmark... {(i / (double)benchmark.Iterations) * 100:F0}%");
+                }
                 Console.SetCursorPosition(position.Left, position.Top);
-                Console.WriteLine($"Running benchmark... {(i / (double)runs) * 100:F0}%");
-            }
-            Console.SetCursorPosition(position.Left, position.Top);
-            Console.WriteLine($"Running benchmark... 100%");
+                Console.WriteLine($"Running benchmark... 100%");
 
-            var average = results.Average();
-            var stdDev = results.StandardDeviation();
-            Console.WriteLine($"Single threaded memory copy, {ThunderscopeMemory.Length} byte block, {runs} runs.");
-            Console.WriteLine($"Mean: {average/1e6:F3} MB/s, StdDev: {stdDev / 1e6:F3} MB/s");
+                var average = results.Average();
+                var stdDev = results.StandardDeviation();
+                Console.WriteLine($"Single threaded memory copy, {benchmark.CopySize} byte block, {benchmark.Iterations} runs.");
+                Console.WriteLine($"Mean: {average / 1e6:F3} MB/s, StdDev: {stdDev / 1e6:F3} MB/s");
+            }
         }
 
         public static double StandardDeviation(this IEnumerable<double> values)
