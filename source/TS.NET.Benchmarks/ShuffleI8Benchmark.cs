8f509349c123597b9a0eca4d9c50ba39ee33855a
macaba <1031306+macaba@users.noreply.github.com>
2024-12-21 22:58:37 +0000

    Updated shuffle benchmark to use NativeMemory

--- source/TS.NET.Benchmarks/ShuffleI8Benchmark.cs
+++ source/TS.NET.Benchmarks/ShuffleI8Benchmark.cs
@@ -1,50 +1,69 @@
-﻿using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Jobs;
+﻿using System.Runtime.InteropServices;
+using BenchmarkDotNet.Attributes;
 
 namespace TS.NET.Benchmarks
 {
-    //[SimpleJob(RuntimeMoniker.Net90)]
     [MemoryDiagnoser]
-    //[CpuDiagnoser]
-    //[InProcess]
-    public class ShuffleI8Benchmark
+    public unsafe class ShuffleI8Benchmark
     {
-        private const int byteBufferSize = 8000000;
-        private readonly Memory<sbyte> input = new sbyte[byteBufferSize];
-        private readonly Memory<sbyte> output = new sbyte[byteBufferSize];
+        private const int byteBufferSize = 1 << 23;
+        private void* inputP;
+        private void* outputP;
 
         [GlobalSetup]
         public void Setup()
         {
-            Waveforms.FourChannelCountSignedByte(input.Span);
+            inputP = NativeMemory.AlignedAlloc(byteBufferSize, 32);
+            outputP = NativeMemory.AlignedAlloc(byteBufferSize, 32);
+            var input = new Span<sbyte>((sbyte*)inputP, byteBufferSize); 
+            Waveforms.FourChannelCountSignedByte(input);
         }
 
-        [Benchmark(Description = "Four channel shuffle, run-length 1 (SIMD, optimised, 125 x 8MS)")]
+        [Benchmark(Description = "Four channel shuffle, run-length 1 (SIMD, optimised)")]
         public void FourChannelsSimdOptimised1G()
         {
-            for (int i = 0; i < 125; i++)
-                ShuffleI8.FourChannels(input.Span, output.Span);
+            var input = new Span<sbyte>((sbyte*)inputP, byteBufferSize); 
+            var output = new Span<sbyte>((sbyte*)outputP, byteBufferSize); 
+            // Processing 120 blocks of 8MiB is an approximation of 1 second of production usage
+            for (int i = 0; i < 120; i++)
+                ShuffleI8.FourChannels(input, output);
         }
 
-        [Benchmark(Description = "Four channel shuffle, run-length 1 (no SIMD, optimised, 125 x 8MS)")]
+        [Benchmark(Description = "Four channel shuffle, run-length 1 (no SIMD, optimised)")]
         public void FourChannelsNoSimdOptimised1G()
         {
-            for (int i = 0; i < 125; i++)
-                ShuffleI8Benchmarks.FourChannelsNoSimd(input.Span, output.Span);
+            var input = new Span<sbyte>((sbyte*)inputP, byteBufferSize); 
+            var output = new Span<sbyte>((sbyte*)outputP, byteBufferSize); 
+            // Processing 120 blocks of 8MiB is an approximation of 1 second of production usage
+            for (int i = 0; i < 120; i++)
+                ShuffleI8Benchmarks.FourChannelsNoSimd(input, output);
         }
 
-        [Benchmark(Description = "Two channel shuffle, run-length 1 (SIMD, optimised, 125 x 8MS)")]
+        [Benchmark(Description = "Two channel shuffle, run-length 1 (SIMD, optimised)")]
         public void TwoChannelsSimdOptimised1G()
         {
-            for (int i = 0; i < 125; i++)
-                ShuffleI8.TwoChannels(input.Span, output.Span);
+            var input = new Span<sbyte>((sbyte*)inputP, byteBufferSize); 
+            var output = new Span<sbyte>((sbyte*)outputP, byteBufferSize); 
+            // Processing 120 blocks of 8MiB is an approximation of 1 second of production usage
+            for (int i = 0; i < 120; i++)
+                ShuffleI8.TwoChannels(input, output);
         }
 
-        [Benchmark(Description = "Two channel shuffle, run-length 1 (no SIMD, optimised, 125 x 8MS)")]
+        [Benchmark(Description = "Two channel shuffle, run-length 1 (no SIMD, optimised)")]
         public void TwoChannelsNoSimdOptimised1G()
         {
-            for (int i = 0; i < 125; i++)
-                ShuffleI8Benchmarks.TwoChannelsNoSimd(input.Span, output.Span);
+            var input = new Span<sbyte>((sbyte*)inputP, byteBufferSize); 
+            var output = new Span<sbyte>((sbyte*)outputP, byteBufferSize); 
+            // Processing 120 blocks of 8MiB is an approximation of 1 second of production usage
+            for (int i = 0; i < 120; i++)
+                ShuffleI8Benchmarks.TwoChannelsNoSimd(input, output);
+        }
+
+        [GlobalCleanup]
+        public void Cleanup()
+        {
+            NativeMemory.Free(inputP);
+            NativeMemory.Free(outputP);
         }
     }
 }
