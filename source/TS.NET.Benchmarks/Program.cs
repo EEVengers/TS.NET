c56fdd75f79904b2531f4ee9c311e7b9b0d94f53
macaba <1031306+macaba@users.noreply.github.com>
2025-01-15 12:02:19 +0000

    Added configs to selectively enable/disable SIMD instruction sets

--- source/TS.NET.Benchmarks/Program.cs
+++ source/TS.NET.Benchmarks/Program.cs
@@ -5,16 +5,33 @@ using BenchmarkDotNet.Running;
 using Perfolizer.Horology;
 using TS.NET.Benchmarks;
 
-bool simd = false;
-var config = ManualConfig.CreateMinimumViable().AddJob(Job.Default
-        .WithEnvironmentVariable("DOTNET_EnableAVX", simd ? "1" : "0")
-        .WithEnvironmentVariable("DOTNET_EnableArm64AdvSimd", simd ? "1" : "0"))
+// https://github.com/dotnet/runtime/blob/main/src/coreclr/inc/clrconfigvalues.h#L718
+
+var scalarConfig = ManualConfig.CreateMinimumViable().AddJob(Job.Default
+        .WithEnvironmentVariable("DOTNET_EnableAVX", "0")
+        .WithEnvironmentVariable("DOTNET_EnableSSSE3", "0")
+        .WithEnvironmentVariable("DOTNET_EnableArm64AdvSimd", "0"))
+    .WithOptions(ConfigOptions.JoinSummary)
+    .WithSummaryStyle(SummaryStyle.Default.WithTimeUnit(TimeUnit.Millisecond));
+
+var simdConfig = ManualConfig.CreateMinimumViable().AddJob(Job.Default
+        .WithEnvironmentVariable("DOTNET_EnableAVX", "1")
+        .WithEnvironmentVariable("DOTNET_EnableSSSE3", "1")
+        .WithEnvironmentVariable("DOTNET_EnableArm64AdvSimd", "1"))
+    .WithOptions(ConfigOptions.JoinSummary)
+    .WithSummaryStyle(SummaryStyle.Default.WithTimeUnit(TimeUnit.Millisecond));
+
+var x64Ssse3Config = ManualConfig.CreateMinimumViable().AddJob(Job.Default
+        .WithEnvironmentVariable("DOTNET_EnableAVX", "0")
+        .WithEnvironmentVariable("DOTNET_EnableSSSE3", "1"))
     .WithOptions(ConfigOptions.JoinSummary)
     .WithSummaryStyle(SummaryStyle.Default.WithTimeUnit(TimeUnit.Millisecond));
 
 BenchmarkRunner.Run([
-    BenchmarkConverter.TypeToBenchmarks(typeof(ShuffleI8Benchmark), config),
-    BenchmarkConverter.TypeToBenchmarks(typeof(RisingEdgeTriggerI8Benchmark), config) 
+    BenchmarkConverter.TypeToBenchmarks(typeof(ShuffleI8Benchmark), scalarConfig),
+    BenchmarkConverter.TypeToBenchmarks(typeof(ShuffleI8Benchmark), x64Ssse3Config),
+    BenchmarkConverter.TypeToBenchmarks(typeof(ShuffleI8Benchmark), simdConfig),
+    //BenchmarkConverter.TypeToBenchmarks(typeof(RisingEdgeTriggerI8Benchmark), scalarConfig) 
     ]);
 //_ = BenchmarkRunner.Run(typeof(Program).Assembly);
 //_ = BenchmarkRunner.Run<MemoryBenchmark>(config);
