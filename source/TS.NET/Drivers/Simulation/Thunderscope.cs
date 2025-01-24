171193f7823544b95bc2c03b87d157ddd138ae2d
macaba <1031306+macaba@users.noreply.github.com>
2025-01-24 19:14:57 +0000

    Working simulator (fixed at 1Mhz sine)

--- source/TS.NET/Drivers/Simulation/Thunderscope.cs
+++ source/TS.NET/Drivers/Simulation/Thunderscope.cs
@@ -2,9 +2,15 @@
 {
     public class Thunderscope : IThunderscope
     {
-        private Memory<sbyte> waveformBytes;
         private DateTimeOffset startTimestamp;
-        private double totalTime;
+        private double totalTimeSec;
+
+        private Memory<sbyte> oneCycle;
+        private float scaleRelativeToFull = 1.0f;
+        private float sampleRateHz = 1e9f;
+        private float frequencyHz = 1e6f;
+        private float phase = 0;
+        private int nextIterationCopy = 0;
 
         public ThunderscopeChannelFrontend GetChannelFrontend(int channelIndex)
         {
@@ -13,32 +19,53 @@
 
         public ThunderscopeHardwareConfig GetConfiguration()
         {
-            var config = new ThunderscopeHardwareConfig() { AdcChannelMode = AdcChannelMode.Quad };
+            var config = new ThunderscopeHardwareConfig()
+            {
+                AdcChannelMode = AdcChannelMode.Single,
+                SampleRateHz = (ulong)sampleRateHz,
+                EnabledChannels = 0x01
+            };
             config.Frontend[0].ActualVoltFullScale = 1;
             config.Frontend[1].ActualVoltFullScale = 1;
             config.Frontend[2].ActualVoltFullScale = 1;
             config.Frontend[3].ActualVoltFullScale = 1;
-            config.SampleRateHz = 250000000;
             return config;
         }
 
         public void Read(ThunderscopeMemory data, CancellationToken cancellationToken)
         {
             // To do: maintain phase counter and then generate or use pre-computed waveform in real-time so frequency can be configurable.
+            var dataSpan = data.SpanI8;
+            var copyLength = dataSpan.Length;
 
-            waveformBytes.Span.CopyTo(data.SpanI8);
-            totalTime += 8.388608;
+            if (nextIterationCopy > 0)
+            {
+                oneCycle.Span.Slice(oneCycle.Length - nextIterationCopy, nextIterationCopy).CopyTo(dataSpan.Slice(0, nextIterationCopy));
+                copyLength -= nextIterationCopy;
+            }
+
+            var fullBufferCopies = copyLength / oneCycle.Length;
+            for (int i = 0; i < fullBufferCopies; i++)
+            {
+                oneCycle.Span.CopyTo(dataSpan.Slice(nextIterationCopy + (i * oneCycle.Length), oneCycle.Length));
+            }
+
+            var remainingCopy = copyLength % oneCycle.Length;
+            oneCycle.Span.Slice(0, remainingCopy).CopyTo(dataSpan.Slice(nextIterationCopy + (fullBufferCopies * oneCycle.Length), remainingCopy));
+
+            nextIterationCopy = oneCycle.Length - remainingCopy;
 
             var duration = DateTime.UtcNow - startTimestamp;
-            var sleepTime = totalTime - duration.TotalMilliseconds;
+            var sleepTime = totalTimeSec - duration.TotalSeconds;
             if (sleepTime < 0)
                 sleepTime = 0;
-            Thread.Sleep((int)sleepTime);
+            Thread.Sleep((int)(sleepTime * 1000));
+            totalTimeSec += data.SpanI8.Length / sampleRateHz;
         }
 
         public void SetChannelFrontend(int channelIndex, ThunderscopeChannelFrontend channel)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public void SetChannelCalibration(int channelIndex, ThunderscopeChannelCalibration channelCalibration)
@@ -48,19 +75,26 @@
 
         public void SetChannelEnable(int channelIndex, bool enabled)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public void Start()
-        {           
-            uint byteBufferSize = ThunderscopeMemory.Length;
-            double samplingRate = 1000000000;
-            double frequency = 476.837158203125; //976562;
-            waveformBytes = new sbyte[byteBufferSize];
-            Waveforms.FourChannelSineI8(waveformBytes.Span, samplingRate, frequency);
-
+        {
             startTimestamp = DateTimeOffset.UtcNow;
-            totalTime = 0;
+            totalTimeSec = 0;
+
+            // Generate a full sine wave at desired sample rate & frequency.          
+            var samplesPerCycle = (int)(sampleRateHz / frequencyHz);
+            var scale = (scaleRelativeToFull * (sbyte.MaxValue - sbyte.MinValue)) * 0.5f;
+            var angularFrequency = (2.0f * MathF.PI * frequencyHz);
+            oneCycle = new sbyte[samplesPerCycle];
+            for (int i = 0; i < oneCycle.Length; i++)
+            {
+                float time = (i / sampleRateHz);
+                float sineValue = MathF.Sin(angularFrequency * time);
+                int scaledValue = (int)(sineValue * scale);
+                oneCycle.Span[i] = (sbyte)scaledValue;
+            }
         }
 
         public void Stop()
