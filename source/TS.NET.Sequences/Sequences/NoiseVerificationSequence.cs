d199bd8f1ccf41b3eeb7e797bda082c7b365949b
macaba <1031306+macaba@users.noreply.github.com>
2025-10-25 19:16:49 +0100

    Added GELE limits to AcRmsStep

--- source/TS.NET.Sequences/Sequences/NoiseVerificationSequence.cs
+++ source/TS.NET.Sequences/Sequences/NoiseVerificationSequence.cs
@@ -30,14 +30,14 @@ public class NoiseVerificationSequence : Sequence
                 return Sequencer.Status.Done;
             }},
 
-            new AcRmsStep("Channel 1 - measure noise AC RMS - 50R, 1 GSPS, 20M, HG L0", 0, 0, ThunderscopeTermination.FiftyOhm, 1_000_000_000, Variables),
-            new AcRmsStep("Channel 1 - measure noise AC RMS - 50R, 500 MSPS, 20M, HG L0", 0, 0, ThunderscopeTermination.FiftyOhm, 500_000_000, Variables),
-            new AcRmsStep("Channel 1 - measure noise AC RMS - 50R, 250 MSPS, 20M, HG L0", 0, 0, ThunderscopeTermination.FiftyOhm, 250_000_000, Variables),
-            new AcRmsStep("Channel 1 - measure noise AC RMS - 50R, 100 MSPS, 20M, HG L0", 0, 0, ThunderscopeTermination.FiftyOhm, 100_000_000, Variables),
-            new AcRmsStep("Channel 1 - measure noise AC RMS - 1M, 1 GSPS, 20M, HG L0", 0, 0, ThunderscopeTermination.OneMegaohm, 1_000_000_000, Variables),
-            new AcRmsStep("Channel 1 - measure noise AC RMS - 1M, 500 MSPS, 20M, HG L0", 0, 0, ThunderscopeTermination.OneMegaohm, 500_000_000, Variables),
-            new AcRmsStep("Channel 1 - measure noise AC RMS - 1M, 250 MSPS, 20M, HG L0", 0, 0, ThunderscopeTermination.OneMegaohm, 250_000_000, Variables),
-            new AcRmsStep("Channel 1 - measure noise AC RMS - 1M, 100 MSPS, 20M, HG L0", 0, 0, ThunderscopeTermination.OneMegaohm, 100_000_000, Variables),
+            new AcRmsStep("Channel 1 - measure noise AC RMS - 50R, 1 GSPS, 20M, HG L0", 0, 0, ThunderscopeTermination.FiftyOhm, 1_000_000_000, Variables){ MinLimit = 0.00003, MaxLimit = 0.00006 },
+            new AcRmsStep("Channel 1 - measure noise AC RMS - 50R, 500 MSPS, 20M, HG L0", 0, 0, ThunderscopeTermination.FiftyOhm, 500_000_000, Variables){ MinLimit = 0.00003, MaxLimit = 0.00006 },
+            new AcRmsStep("Channel 1 - measure noise AC RMS - 50R, 250 MSPS, 20M, HG L0", 0, 0, ThunderscopeTermination.FiftyOhm, 250_000_000, Variables){ MinLimit = 0.00003, MaxLimit = 0.00006 },
+            new AcRmsStep("Channel 1 - measure noise AC RMS - 50R, 100 MSPS, 20M, HG L0", 0, 0, ThunderscopeTermination.FiftyOhm, 100_000_000, Variables){ MinLimit = 0.00003, MaxLimit = 0.00006 },
+            new AcRmsStep("Channel 1 - measure noise AC RMS - 1M, 1 GSPS, 20M, HG L0", 0, 0, ThunderscopeTermination.OneMegaohm, 1_000_000_000, Variables){ MinLimit = 0.00005, MaxLimit = 0.00008 },
+            new AcRmsStep("Channel 1 - measure noise AC RMS - 1M, 500 MSPS, 20M, HG L0", 0, 0, ThunderscopeTermination.OneMegaohm, 500_000_000, Variables){ MinLimit = 0.00005, MaxLimit = 0.00008 },
+            new AcRmsStep("Channel 1 - measure noise AC RMS - 1M, 250 MSPS, 20M, HG L0", 0, 0, ThunderscopeTermination.OneMegaohm, 250_000_000, Variables){ MinLimit = 0.00005, MaxLimit = 0.00008 },
+            new AcRmsStep("Channel 1 - measure noise AC RMS - 1M, 100 MSPS, 20M, HG L0", 0, 0, ThunderscopeTermination.OneMegaohm, 100_000_000, Variables){ MinLimit = 0.00005, MaxLimit = 0.00008 },
 
             new Step("Set channel 2"){ Action = (CancellationToken cancellationToken) => {
                 Instruments.Instance.SetThunderscopeChannel([1]);
@@ -45,14 +45,14 @@ public class NoiseVerificationSequence : Sequence
                 return Sequencer.Status.Done;
             }},
 
-            new AcRmsStep("Channel 2 - measure noise AC RMS - 50R, 1 GSPS, 20M, HG L0", 1, 0, ThunderscopeTermination.FiftyOhm, 1_000_000_000, Variables),
-            new AcRmsStep("Channel 2 - measure noise AC RMS - 50R, 500 MSPS, 20M, HG L0", 1, 0, ThunderscopeTermination.FiftyOhm, 500_000_000, Variables),
-            new AcRmsStep("Channel 2 - measure noise AC RMS - 50R, 250 MSPS, 20M, HG L0", 1, 0, ThunderscopeTermination.FiftyOhm, 250_000_000, Variables),
-            new AcRmsStep("Channel 2 - measure noise AC RMS - 50R, 100 MSPS, 20M, HG L0", 1, 0, ThunderscopeTermination.FiftyOhm, 100_000_000, Variables),
-            new AcRmsStep("Channel 2 - measure noise AC RMS - 1M, 1 GSPS, 20M, HG L0", 1, 0, ThunderscopeTermination.OneMegaohm, 1_000_000_000, Variables),
-            new AcRmsStep("Channel 2 - measure noise AC RMS - 1M, 500 MSPS, 20M, HG L0", 1, 0, ThunderscopeTermination.OneMegaohm, 500_000_000, Variables),
-            new AcRmsStep("Channel 2 - measure noise AC RMS - 1M, 250 MSPS, 20M, HG L0", 1, 0, ThunderscopeTermination.OneMegaohm, 250_000_000, Variables),
-            new AcRmsStep("Channel 2 - measure noise AC RMS - 1M, 100 MSPS, 20M, HG L0", 1, 0, ThunderscopeTermination.OneMegaohm, 100_000_000, Variables),
+            new AcRmsStep("Channel 2 - measure noise AC RMS - 50R, 1 GSPS, 20M, HG L0", 1, 0, ThunderscopeTermination.FiftyOhm, 1_000_000_000, Variables){ MinLimit = 0.00003, MaxLimit = 0.00006 },
+            new AcRmsStep("Channel 2 - measure noise AC RMS - 50R, 500 MSPS, 20M, HG L0", 1, 0, ThunderscopeTermination.FiftyOhm, 500_000_000, Variables){ MinLimit = 0.00003, MaxLimit = 0.00006 },
+            new AcRmsStep("Channel 2 - measure noise AC RMS - 50R, 250 MSPS, 20M, HG L0", 1, 0, ThunderscopeTermination.FiftyOhm, 250_000_000, Variables){ MinLimit = 0.00003, MaxLimit = 0.00006 },
+            new AcRmsStep("Channel 2 - measure noise AC RMS - 50R, 100 MSPS, 20M, HG L0", 1, 0, ThunderscopeTermination.FiftyOhm, 100_000_000, Variables){ MinLimit = 0.00003, MaxLimit = 0.00006 },
+            new AcRmsStep("Channel 2 - measure noise AC RMS - 1M, 1 GSPS, 20M, HG L0", 1, 0, ThunderscopeTermination.OneMegaohm, 1_000_000_000, Variables){ MinLimit = 0.00005, MaxLimit = 0.00008 },
+            new AcRmsStep("Channel 2 - measure noise AC RMS - 1M, 500 MSPS, 20M, HG L0", 1, 0, ThunderscopeTermination.OneMegaohm, 500_000_000, Variables){ MinLimit = 0.00005, MaxLimit = 0.00008 },
+            new AcRmsStep("Channel 2 - measure noise AC RMS - 1M, 250 MSPS, 20M, HG L0", 1, 0, ThunderscopeTermination.OneMegaohm, 250_000_000, Variables){ MinLimit = 0.00005, MaxLimit = 0.00008 },
+            new AcRmsStep("Channel 2 - measure noise AC RMS - 1M, 100 MSPS, 20M, HG L0", 1, 0, ThunderscopeTermination.OneMegaohm, 100_000_000, Variables){ MinLimit = 0.00005, MaxLimit = 0.00008 },
 
             new Step("Set channel 3"){ Action = (CancellationToken cancellationToken) => {
                 Instruments.Instance.SetThunderscopeChannel([2]);
@@ -60,14 +60,14 @@ public class NoiseVerificationSequence : Sequence
                 return Sequencer.Status.Done;
             }},
 
-            new AcRmsStep("Channel 3 - measure noise AC RMS - 50R, 1 GSPS, 20M, HG L0", 2, 0, ThunderscopeTermination.FiftyOhm, 1_000_000_000, Variables),
-            new AcRmsStep("Channel 3 - measure noise AC RMS - 50R, 500 MSPS, 20M, HG L0", 2, 0, ThunderscopeTermination.FiftyOhm, 500_000_000, Variables),
-            new AcRmsStep("Channel 3 - measure noise AC RMS - 50R, 250 MSPS, 20M, HG L0", 2, 0, ThunderscopeTermination.FiftyOhm, 250_000_000, Variables),
-            new AcRmsStep("Channel 3 - measure noise AC RMS - 50R, 100 MSPS, 20M, HG L0", 2, 0, ThunderscopeTermination.FiftyOhm, 100_000_000, Variables),
-            new AcRmsStep("Channel 3 - measure noise AC RMS - 1M, 1 GSPS, 20M, HG L0", 2, 0, ThunderscopeTermination.OneMegaohm, 1_000_000_000, Variables),
-            new AcRmsStep("Channel 3 - measure noise AC RMS - 1M, 500 MSPS, 20M, HG L0", 2, 0, ThunderscopeTermination.OneMegaohm, 500_000_000, Variables),
-            new AcRmsStep("Channel 3 - measure noise AC RMS - 1M, 250 MSPS, 20M, HG L0", 2, 0, ThunderscopeTermination.OneMegaohm, 250_000_000, Variables),
-            new AcRmsStep("Channel 3 - measure noise AC RMS - 1M, 100 MSPS, 20M, HG L0", 2, 0, ThunderscopeTermination.OneMegaohm, 100_000_000, Variables),
+            new AcRmsStep("Channel 3 - measure noise AC RMS - 50R, 1 GSPS, 20M, HG L0", 2, 0, ThunderscopeTermination.FiftyOhm, 1_000_000_000, Variables){ MinLimit = 0.00003, MaxLimit = 0.00006 },
+            new AcRmsStep("Channel 3 - measure noise AC RMS - 50R, 500 MSPS, 20M, HG L0", 2, 0, ThunderscopeTermination.FiftyOhm, 500_000_000, Variables){ MinLimit = 0.00003, MaxLimit = 0.00006 },
+            new AcRmsStep("Channel 3 - measure noise AC RMS - 50R, 250 MSPS, 20M, HG L0", 2, 0, ThunderscopeTermination.FiftyOhm, 250_000_000, Variables){ MinLimit = 0.00003, MaxLimit = 0.00006 },
+            new AcRmsStep("Channel 3 - measure noise AC RMS - 50R, 100 MSPS, 20M, HG L0", 2, 0, ThunderscopeTermination.FiftyOhm, 100_000_000, Variables){ MinLimit = 0.00003, MaxLimit = 0.00006 },
+            new AcRmsStep("Channel 3 - measure noise AC RMS - 1M, 1 GSPS, 20M, HG L0", 2, 0, ThunderscopeTermination.OneMegaohm, 1_000_000_000, Variables){ MinLimit = 0.00005, MaxLimit = 0.00008 },
+            new AcRmsStep("Channel 3 - measure noise AC RMS - 1M, 500 MSPS, 20M, HG L0", 2, 0, ThunderscopeTermination.OneMegaohm, 500_000_000, Variables){ MinLimit = 0.00005, MaxLimit = 0.00008 },
+            new AcRmsStep("Channel 3 - measure noise AC RMS - 1M, 250 MSPS, 20M, HG L0", 2, 0, ThunderscopeTermination.OneMegaohm, 250_000_000, Variables){ MinLimit = 0.00005, MaxLimit = 0.00008 },
+            new AcRmsStep("Channel 3 - measure noise AC RMS - 1M, 100 MSPS, 20M, HG L0", 2, 0, ThunderscopeTermination.OneMegaohm, 100_000_000, Variables){ MinLimit = 0.00005, MaxLimit = 0.00008 },
 
             new Step("Set channel 4"){ Action = (CancellationToken cancellationToken) => {
                 Instruments.Instance.SetThunderscopeChannel([3]);
@@ -75,14 +75,14 @@ public class NoiseVerificationSequence : Sequence
                 return Sequencer.Status.Done;
             }},
 
-            new AcRmsStep("Channel 4 - measure noise AC RMS - 50R, 1 GSPS, 20M, HG L0", 3, 0, ThunderscopeTermination.FiftyOhm, 1_000_000_000, Variables),
-            new AcRmsStep("Channel 4 - measure noise AC RMS - 50R, 500 MSPS, 20M, HG L0", 3, 0, ThunderscopeTermination.FiftyOhm, 500_000_000, Variables),
-            new AcRmsStep("Channel 4 - measure noise AC RMS - 50R, 250 MSPS, 20M, HG L0", 3, 0, ThunderscopeTermination.FiftyOhm, 250_000_000, Variables),
-            new AcRmsStep("Channel 4 - measure noise AC RMS - 50R, 100 MSPS, 20M, HG L0", 3, 0, ThunderscopeTermination.FiftyOhm, 100_000_000, Variables),
-            new AcRmsStep("Channel 4 - measure noise AC RMS - 1M, 1 GSPS, 20M, HG L0", 3, 0, ThunderscopeTermination.OneMegaohm, 1_000_000_000, Variables),
-            new AcRmsStep("Channel 4 - measure noise AC RMS - 1M, 500 MSPS, 20M, HG L0", 3, 0, ThunderscopeTermination.OneMegaohm, 500_000_000, Variables),
-            new AcRmsStep("Channel 4 - measure noise AC RMS - 1M, 250 MSPS, 20M, HG L0", 3, 0, ThunderscopeTermination.OneMegaohm, 250_000_000, Variables),
-            new AcRmsStep("Channel 4 - measure noise AC RMS - 1M, 100 MSPS, 20M, HG L0", 3, 0, ThunderscopeTermination.OneMegaohm, 100_000_000, Variables),
+            new AcRmsStep("Channel 4 - measure noise AC RMS - 50R, 1 GSPS, 20M, HG L0", 3, 0, ThunderscopeTermination.FiftyOhm, 1_000_000_000, Variables){ MinLimit = 0.00003, MaxLimit = 0.00006 },
+            new AcRmsStep("Channel 4 - measure noise AC RMS - 50R, 500 MSPS, 20M, HG L0", 3, 0, ThunderscopeTermination.FiftyOhm, 500_000_000, Variables){ MinLimit = 0.00003, MaxLimit = 0.00006 },
+            new AcRmsStep("Channel 4 - measure noise AC RMS - 50R, 250 MSPS, 20M, HG L0", 3, 0, ThunderscopeTermination.FiftyOhm, 250_000_000, Variables){ MinLimit = 0.00003, MaxLimit = 0.00006 },
+            new AcRmsStep("Channel 4 - measure noise AC RMS - 50R, 100 MSPS, 20M, HG L0", 3, 0, ThunderscopeTermination.FiftyOhm, 100_000_000, Variables){ MinLimit = 0.00003, MaxLimit = 0.00006 },
+            new AcRmsStep("Channel 4 - measure noise AC RMS - 1M, 1 GSPS, 20M, HG L0", 3, 0, ThunderscopeTermination.OneMegaohm, 1_000_000_000, Variables){ MinLimit = 0.00005, MaxLimit = 0.00008 },
+            new AcRmsStep("Channel 4 - measure noise AC RMS - 1M, 500 MSPS, 20M, HG L0", 3, 0, ThunderscopeTermination.OneMegaohm, 500_000_000, Variables){ MinLimit = 0.00005, MaxLimit = 0.00008 },
+            new AcRmsStep("Channel 4 - measure noise AC RMS - 1M, 250 MSPS, 20M, HG L0", 3, 0, ThunderscopeTermination.OneMegaohm, 250_000_000, Variables){ MinLimit = 0.00005, MaxLimit = 0.00008 },
+            new AcRmsStep("Channel 4 - measure noise AC RMS - 1M, 100 MSPS, 20M, HG L0", 3, 0, ThunderscopeTermination.OneMegaohm, 100_000_000, Variables){ MinLimit = 0.00005, MaxLimit = 0.00008 },
 
             new Step("Cleanup"){ Action = (CancellationToken cancellationToken) => {
                 Instruments.Instance.Close();
--- source/TS.NET.Sequences/Steps/AcRmsStep.cs
+++ source/TS.NET.Sequences/Steps/AcRmsStep.cs
@@ -4,12 +4,15 @@ namespace TS.NET.Sequences;
 
 public class AcRmsStep : Step
 {
+    public double MinLimit { get; set; } = double.MinValue;
+    public double MaxLimit { get; set; } = double.MaxValue;
+
     public AcRmsStep(string name, int channelIndex, int pathIndex, ThunderscopeTermination termination, uint rateHz, CommonVariables variables) : base(name)
     {
         Action = (CancellationToken cancellationToken) =>
         {
             var pathCalibration = Utility.GetChannelPathCalibration(channelIndex, pathIndex, variables);
-            switch(termination)
+            switch (termination)
             {
                 case ThunderscopeTermination.FiftyOhm:
                     Instruments.Instance.SetThunderscopeCalManual50R(channelIndex, pathCalibration.TrimOffsetDacZero, pathCalibration.TrimScaleDac, pathCalibration.PgaPreampGain, pathCalibration.PgaLadderAttenuator, variables);
@@ -23,8 +26,17 @@ public class AcRmsStep : Step
             var stdDev = Instruments.Instance.GetThunderscopePopulationStdDev(channelIndex);
             var stdDevV = stdDev * (pathCalibration.BufferInputVpp / 255.0);
 
-            Logger.Instance.Log(LogLevel.Information, Index, Status.Passed, $"uVrms: {stdDevV * 1e6:F4}");
-            return Status.Passed;
+            if (stdDevV >= MinLimit && stdDevV <= MaxLimit)
+            {
+                Logger.Instance.Log(LogLevel.Information, Index, Status.Passed, $"uVrms: {stdDevV * 1e6:F4}");
+                return Status.Passed;
+            }
+            else
+            {
+                Logger.Instance.Log(LogLevel.Information, Index, Status.Failed, $"uVrms: {stdDevV * 1e6:F4}");
+                return Status.Failed;
+            }
+            
         };
     }
 }
