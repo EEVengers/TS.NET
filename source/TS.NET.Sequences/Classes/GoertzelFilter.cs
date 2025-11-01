6a0188a6e8e7600c3ff394b52fca63c48440017d
macaba <1031306+macaba@users.noreply.github.com>
2025-11-01 16:01:55 +0000

    Create GoertzelFilter.cs

---
+++ source/TS.NET.Sequences/Classes/GoertzelFilter.cs
@@ -0,0 +1,36 @@
+﻿using System.Numerics;
+
+namespace TS.NET.Sequences;
+
+public class GoertzelFilter
+{
+    private readonly double coeff;
+    private readonly double sine;
+    private readonly double cosine;
+
+    public GoertzelFilter(double filterFreq, double sampleFreq)
+    {
+        double w = 2.0 * Math.PI * (filterFreq / sampleFreq);
+        cosine = Math.Cos(w);
+        sine = Math.Sin(w);
+        coeff = 2.0 * cosine;
+    }
+
+    public Complex Process(Span<double> samples)
+    {
+        double Q0 = 0.0;
+        double Q1 = 0.0;
+        double Q2 = 0.0;
+
+        for (int n = 0; n < samples.Length; n++)
+        {
+            Q0 = coeff * Q1 - Q2 + samples[n];
+            Q2 = Q1;
+            Q1 = Q0;
+        }
+
+        var real = Q1 * cosine - Q2;
+        var imag = -Q1 * sine;
+        return new Complex(real, imag);
+    }
+}
