2e14089a74c7e40b20756d77a4a6a1a9d937612c
macaba <1031306+macaba@users.noreply.github.com>
2026-06-08 18:52:03 +0100

    Factory trim sequence update

--- source/TS.NET.Sequences/Sequences/FactoryTrimSequence.cs
+++ source/TS.NET.Sequences/Sequences/FactoryTrimSequence.cs
@@ -38,45 +38,90 @@ public class FactoryTrimSequence : Sequence
             },
             new WarmupStep("Warmup device", Variables, 40 * 60)
             {
-                Skip = false,
+                Skip = true,
                 AllowSkip = true
             },
 
-            new TrimPotStep("Channel 1 - Trim pot", modalUiContext, Variables)
+            new TrimStep("Channel 1 - Trim pot", modalUiContext, Variables)
             {
                 ChannelIndex = 0,
                 ChannelsEnabled = [0],
                 PgaPreampGain = PgaPreampGain.Low,
                 PgaLadder = 10,
                 Attenuator = false,
-                SampleRateHz = 1_000_000_000
+                SampleRateHz = 1_000_000_000,
+                BodeFrequencies = [500, 1_000, 2_000, 4_000, 7_000, 10_000, 20_000, 40_000, 70_000, 100_000, 200_000, 400_000, 700_000, 990_000]
             },
-            new TrimPotStep("Channel 2 - Trim pot", modalUiContext, Variables)
+            new TrimStep("Channel 2 - Trim pot", modalUiContext, Variables)
             {
                 ChannelIndex = 1,
                 ChannelsEnabled = [1],
                 PgaPreampGain = PgaPreampGain.Low,
                 PgaLadder = 10,
                 Attenuator = false,
-                SampleRateHz = 1_000_000_000
+                SampleRateHz = 1_000_000_000,
+                BodeFrequencies = [500, 1_000, 2_000, 4_000, 7_000, 10_000, 20_000, 40_000, 70_000, 100_000, 200_000, 400_000, 700_000, 990_000]
             },
-            new TrimPotStep("Channel 3 - Trim pot", modalUiContext, Variables)
+            new TrimStep("Channel 3 - Trim pot", modalUiContext, Variables)
             {
                 ChannelIndex = 2,
                 ChannelsEnabled = [2],
                 PgaPreampGain = PgaPreampGain.Low,
                 PgaLadder = 10,
                 Attenuator = false,
-                SampleRateHz = 1_000_000_000
+                SampleRateHz = 1_000_000_000,
+                BodeFrequencies = [500, 1_000, 2_000, 4_000, 7_000, 10_000, 20_000, 40_000, 70_000, 100_000, 200_000, 400_000, 700_000, 990_000]
             },
-            new TrimPotStep("Channel 4 - Trim pot", modalUiContext, Variables)
+            new TrimStep("Channel 4 - Trim pot", modalUiContext, Variables)
             {
                 ChannelIndex = 3,
                 ChannelsEnabled = [3],
                 PgaPreampGain = PgaPreampGain.Low,
                 PgaLadder = 10,
                 Attenuator = false,
-                SampleRateHz = 1_000_000_000
+                SampleRateHz = 1_000_000_000,
+                BodeFrequencies = [500, 1_000, 2_000, 4_000, 7_000, 10_000, 20_000, 40_000, 70_000, 100_000, 200_000, 400_000, 700_000, 990_000]
+            },
+
+            new TrimStep("Channel 1 - Trim cap", modalUiContext, Variables)
+            {
+                ChannelIndex = 0,
+                ChannelsEnabled = [0],
+                PgaPreampGain = PgaPreampGain.High,
+                PgaLadder = 10,
+                Attenuator = true,
+                SampleRateHz = 1_000_000_000,
+                BodeFrequencies = [5_000, 7_000, 10_000, 20_000, 40_000, 70_000, 100_000, 200_000, 400_000, 700_000, 1_000_000, 2_000_000, 4_000_000, 7_000_000, 10_000_000]
+            },
+            new TrimStep("Channel 2 - Trim cap", modalUiContext, Variables)
+            {
+                ChannelIndex = 1,
+                ChannelsEnabled = [1],
+                PgaPreampGain = PgaPreampGain.High,
+                PgaLadder = 10,
+                Attenuator = true,
+                SampleRateHz = 1_000_000_000,
+                BodeFrequencies = [5_000, 7_000, 10_000, 20_000, 40_000, 70_000, 100_000, 200_000, 400_000, 700_000, 1_000_000, 2_000_000, 4_000_000, 7_000_000, 10_000_000]
+            },
+            new TrimStep("Channel 3 - Trim cap", modalUiContext, Variables)
+            {
+                ChannelIndex = 2,
+                ChannelsEnabled = [2],
+                PgaPreampGain = PgaPreampGain.High,
+                PgaLadder = 10,
+                Attenuator = true,
+                SampleRateHz = 1_000_000_000,
+                BodeFrequencies = [5_000, 7_000, 10_000, 20_000, 40_000, 70_000, 100_000, 200_000, 400_000, 700_000, 1_000_000, 2_000_000, 4_000_000, 7_000_000, 10_000_000]
+            },
+            new TrimStep("Channel 4 - Trim cap", modalUiContext, Variables)
+            {
+                ChannelIndex = 3,
+                ChannelsEnabled = [3],
+                PgaPreampGain = PgaPreampGain.High,
+                PgaLadder = 10,
+                Attenuator = true,
+                SampleRateHz = 1_000_000_000,
+                BodeFrequencies = [5_000, 7_000, 10_000, 20_000, 40_000, 70_000, 100_000, 200_000, 400_000, 700_000, 1_000_000, 2_000_000, 4_000_000, 7_000_000, 10_000_000]
             },
 
             new Step("Cleanup")
--- source/TS.NET.Sequences/Steps/TrimStep/TrimStep.cs
+++ source/TS.NET.Sequences/Steps/TrimStep/TrimStep.cs
@@ -1,11 +1,12 @@
-﻿using System.Reflection;
+﻿using System.Diagnostics;
+using System.Reflection;
 using System.Text;
 using System.Text.Json;
 using TS.NET.Sequencer;
 
 namespace TS.NET.Sequences;
 
-public class TrimPotStep : ModalUiStep
+public class TrimStep : ModalUiStep
 {
     public required int ChannelIndex { get; set; }
     public required int[] ChannelsEnabled { get; set; }
@@ -13,11 +14,12 @@ public class TrimPotStep : ModalUiStep
     public int PgaLadder { get; set; }
     public bool Attenuator { get; set; }
     public required uint SampleRateHz { get; set; }
+    public required uint[] BodeFrequencies { get; set; }
 
     private Status status = Status.Running;
     private bool continueLoop = false;
 
-    public TrimPotStep(string name, ModalUiContext modalUiContext, CommonVariables variables) : base(name, modalUiContext)
+    public TrimStep(string name, ModalUiContext modalUiContext, CommonVariables variables) : base(name, modalUiContext)
     {
         Action = (CancellationToken cancellationToken) =>
         {
@@ -46,8 +48,12 @@ public class TrimPotStep : ModalUiStep
             Instruments.Instance.SetThunderscopeResolution(resolution);
             Instruments.Instance.SetThunderscopeRate(SampleRateHz);
             var pathCalibration = Utility.GetChannelPathCalibration(ChannelIndex, PgaPreampGain, PgaLadder, variables);
-            double attenuatorScale = variables.Calibration.Frontend[ChannelIndex].AttenuatorScale;
-            double amplitudeVpp = pathCalibration.BufferInputVpp * 0.8 / (Attenuator ? attenuatorScale : 1.0);
+            var scaleFactor = Attenuator ? variables.Calibration.Frontend[ChannelIndex].AttenuatorScale : 1.0;
+            double amplitudeVpp = pathCalibration.BufferInputVpp * 0.8 * scaleFactor;
+            if (amplitudeVpp > 5.0)
+            {
+                amplitudeVpp = 5.0;
+            }
             SigGens.Instance.SetSdgChannel([ChannelIndex]);
             SigGens.Instance.SetSdgLoad(ChannelIndex, ThunderscopeTermination.FiftyOhm);
             SigGens.Instance.SetSdgSine(ChannelIndex);
@@ -58,27 +64,28 @@ public class TrimPotStep : ModalUiStep
             var trimDacZero = Frontend.GetTrimDacZero(temperature, pathCalibration.TrimDacZeroM, pathCalibration.TrimDacZeroC);
             Instruments.Instance.SetThunderscopeCalManual50R(ChannelIndex, Attenuator, trimDacZero, pathCalibration.TrimDPot, pathCalibration.PgaPreampGain, pathCalibration.PgaLadder, ThunderscopeBandwidth.BwFull, variables.FrontEndSettlingTimeMs);
 
-            uint[] frequenciesHz = [500, 1_000, 2_000, 4_000, 7_000, 10_000, 20_000, 40_000, 70_000, 100_000, 200_000, 400_000, 700_000, 990_000];
-
             continueLoop = true;
             while (continueLoop)
             {
+                var stopwatch = Stopwatch.StartNew();
                 cancellationToken.ThrowIfCancellationRequested();
 
                 var bodePoints = new Dictionary<uint, double>();
-                SigGens.Instance.SetSdgParameterFrequency(ChannelIndex, frequenciesHz[0]);
-                var signalAtRef = Instruments.Instance.GetThunderscopeVppAtFrequencyLsq(ChannelIndex, frequenciesHz[0], SampleRateHz, pathCalibration.BufferInputVpp, resolution, out float rangePercentAtRef) / (Attenuator ? attenuatorScale : 1.0);
-                bodePoints[frequenciesHz[0]] = 1.0;
-                for (int i = 1; i < frequenciesHz.Length && continueLoop; i++)
-                {
+                SigGens.Instance.SetSdgParameterFrequency(ChannelIndex, BodeFrequencies[0]);
+                var signalAtRef = Instruments.Instance.GetThunderscopeVppAtFrequencyLsq(ChannelIndex, BodeFrequencies[0], SampleRateHz, pathCalibration.BufferInputVpp, resolution, out float rangePercentAtRef) * scaleFactor;
+                bodePoints[BodeFrequencies[0]] = 1.0;
+                for (int i = 1; i < BodeFrequencies.Length && continueLoop; i++)
+                {                   
                     cancellationToken.ThrowIfCancellationRequested();
-                    SigGens.Instance.SetSdgParameterFrequency(ChannelIndex, frequenciesHz[i]);
-                    var signalAtFrequency = Instruments.Instance.GetThunderscopeVppAtFrequencyLsq(ChannelIndex, frequenciesHz[i], SampleRateHz, pathCalibration.BufferInputVpp, resolution, out float rangePercent) / (Attenuator ? attenuatorScale : 1.0);
+                    SigGens.Instance.SetSdgParameterFrequency(ChannelIndex, BodeFrequencies[i]);
+                    var signalAtFrequency = Instruments.Instance.GetThunderscopeVppAtFrequencyLsq(ChannelIndex, BodeFrequencies[i], SampleRateHz, pathCalibration.BufferInputVpp, resolution, out float rangePercent) * scaleFactor;
                     // Normalise relative to the reference measurement
                     double normalised = signalAtFrequency / signalAtRef;
-                    bodePoints[frequenciesHz[i]] = normalised;
-                }
+                    bodePoints[BodeFrequencies[i]] = normalised;
 
+                }
+                stopwatch.Stop();
+                Debug.WriteLine($"Bode measurement loop took {stopwatch.Elapsed.TotalSeconds:F2} seconds");
                 var metadata =
                     new ResultMetadataXYChart()
                     {
@@ -101,9 +108,9 @@ public class TrimPotStep : ModalUiStep
                         ]
                     };
 
-                UpdateUi<TrimPot>(new Dictionary<string, object?>()
+                UpdateUi<Trim>(new Dictionary<string, object?>()
                 {
-                    { "Title", "Channel 1 - Trim pot"},
+                    { "Title", name},
                     { "Chart", metadata }
                 });
 
